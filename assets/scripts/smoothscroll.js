880bcba7947b19aa56fcfade5fdd6295b00c29e4
Vitris <tug24657@temple.edu>
2018-09-20 16:23:51 -0400

    update smoothscroll and carsousel

--- assets/scripts/smoothscroll.js
+++ assets/scripts/smoothscroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $('a:not([href="#vitrisCarousel"])').on('click', function(event) {
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
