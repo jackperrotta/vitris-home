2865f8042da427bddb9ec0546a1511c29c14460c
Vitris <tug24657@temple.edu>
2018-09-25 22:50:52 -0400

    finish new home page header new pricing header and website section of features banner

--- assets/scripts/random-number.js
+++ assets/scripts/random-number.js
@@ -1,10 +1,34 @@
 var number = Math.floor(Math.random() * 4) + 1;  // returns a random integer from 1 to 4
-console.log(number);
-getClass(number);
 
-function getClass(number){
+var header1 = document.getElementsByClassName("header-1");
+var header2 = document.getElementsByClassName("header-2");
+var header3 = document.getElementsByClassName("header-3");
+var header4 = document.getElementsByClassName("header-4");
+
+setClasses();
+
+function setClasses(){
     var selectedClass = document.getElementsByClassName("header-"+number);
-    console.log(selectedClass);
-    selectedClass.style.display = 'flex';
-    
+    if(header1 === selectedClass){
+        selectedClass[0].style.display = 'flex';
+        header2[0].style.display = 'none'
+        header3[0].style.display = 'none'
+        header4[0].style.display = 'none'
+
+    } else if(header2 === selectedClass){
+        selectedClass[0].style.display = 'flex';
+        header1[0].style.display = 'none'
+        header3[0].style.display = 'none'
+        header4[0].style.display = 'none'
+    } else if(header3 === selectedClass){
+        selectedClass[0].style.display = 'flex';
+        header1[0].style.display = 'none'
+        header2[0].style.display = 'none'
+        header4[0].style.display = 'none'
+    } else if(header4 === selectedClass){
+        selectedClass[0].style.display = 'flex';
+        header1[0].style.display = 'none'
+        header2[0].style.display = 'none'
+        header3[0].style.display = 'none'
+    }
 }
