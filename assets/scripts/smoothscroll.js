c1f561acd64b78d4bb31a6c15c26cdfa587b8e5d
Vitris <tug24657@temple.edu>
2018-11-14 10:31:35 -0500

    remove jesses testimonial video

--- assets/scripts/smoothscroll.js
+++ assets/scripts/smoothscroll.js
@@ -9,15 +9,17 @@ $(document).ready(function(){
   
         // Store hash
         var hash = this.hash;
-  
+        
+        var targetOffset = $(hash).offset().top-105;
+
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top - 105
+          scrollTop: targetOffset
         }, 950, function(){
-     
+          
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+          window.location.hash = targetOffset;
         });
       } // End if
     });
