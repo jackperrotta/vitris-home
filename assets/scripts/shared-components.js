5f52882e845deea7bd288d2e9b7502fb18262e32
jackperrotta <tug24657@temple.edu>
2019-02-02 15:30:44 -0500

    update footer links with ga

--- assets/scripts/shared-components.js
+++ assets/scripts/shared-components.js
@@ -1,4 +1,8 @@
 $(function(){
-    $("#nav").load("https://tryvitris.com/assets/shared-components/nav.html"); 
-    $("#footer").load("https://tryvitris.com/assets/shared-components/footer.html"); 
+    // Prod
+    // $("#nav").load("https://tryvitris.com/assets/shared-components/nav.html"); 
+    // $("#footer").load("https://tryvitris.com/assets/shared-components/footer.html");
+    // Dev
+    $("#nav").load("http://127.0.0.1:8080/assets/shared-components/nav.html"); 
+    $("#footer").load("http://127.0.0.1:8080/assets/shared-components/footer.html"); 
 });
