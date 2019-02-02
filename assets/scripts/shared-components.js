58d9fd5d0838c3b77e22969fecb1ede9c1a8cc04
jackperrotta <tug24657@temple.edu>
2019-02-02 14:39:42 -0500

    add shared components docs, shared script, and nav and footer divs to each page

---
+++ assets/scripts/shared-components.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#nav").load("https://tryvitris.com/assets/shared-components/nav.html"); 
+    $("#footer").load("https://tryvitris.com/assets/shared-components/footer.html"); 
+});
