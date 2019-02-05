a6ff91fa249688723bdc9d687dfb2ee847ed7ace
jackperrotta <tug24657@temple.edu>
2019-02-05 18:40:36 -0500

    started playing with bottom nav on mobile and added portfolio page

--- assets/scripts/shared-components.js
+++ assets/scripts/shared-components.js
@@ -1,8 +1,8 @@
 $(function(){
     // Prod
-    $("#nav").load("https://tryvitris.com/assets/shared-components/nav.html"); 
-    $("#footer").load("https://tryvitris.com/assets/shared-components/footer.html");
+    // $("#nav").load("https://tryvitris.com/assets/shared-components/nav.html"); 
+    // $("#footer").load("https://tryvitris.com/assets/shared-components/footer.html");
     // Dev
-    // $("#nav").load("http://127.0.0.1:8080/assets/shared-components/nav.html"); 
-    // $("#footer").load("http://127.0.0.1:8080/assets/shared-components/footer.html"); 
+    $("#nav").load("http://127.0.0.1:8080/assets/shared-components/nav.html"); 
+    $("#footer").load("http://127.0.0.1:8080/assets/shared-components/footer.html"); 
 });
