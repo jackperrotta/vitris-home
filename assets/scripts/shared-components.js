1d795953082e469bfe4a873d5e619d84a96b693c
jackperrotta <tug24657@temple.edu>
2019-02-12 14:38:04 -0500

    change shared modal for all contact modal requests and add shared modal div to all pages

--- assets/scripts/shared-components.js
+++ assets/scripts/shared-components.js
@@ -2,4 +2,5 @@ $(function(){
     var basepath = window.location.protocol + "//" + window.location.host + "/";
     $("#nav").load(basepath + "assets/shared-components/nav.html"); 
     $("#footer").load(basepath + "assets/shared-components/footer.html"); 
+    $("#contact-modal").load(basepath + "assets/shared-components/contact-modal.html"); 
 });
