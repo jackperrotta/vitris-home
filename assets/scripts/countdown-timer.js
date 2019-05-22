15f3d163b928dcadeaaa98107e41e1e9a9cc8f4e
jackperrotta <tug24657@temple.edu>
2019-05-22 10:43:31 -0400

    add tawk chat

--- assets/scripts/countdown-timer.js
+++ assets/scripts/countdown-timer.js
@@ -1,5 +1,5 @@
  // Set the date we're counting down to
- var countDownDate = new Date("July 1, 2019 12:00:00").getTime();
+ var countDownDate = new Date("June 30, 2019 12:00:00").getTime();
 
  // Update the count down every 1 second
  var x = setInterval(function() {
@@ -28,6 +28,6 @@
  // If the count down is finished, write some text
  if (distance < 0) {
      clearInterval(x);
-     document.getElementById("timer").innerHTML = "Let's Get Started";
+     document.getElementById("timer").innerHTML = "The deal has ended :(";
  }
  }, 1000);
--- assets/scripts/shared-components.js
+++ assets/scripts/shared-components.js
@@ -4,3 +4,23 @@ $(function(){
     $("#footer").load(basepath + "assets/shared-components/footer.html"); 
     $("#contact-modal").load(basepath + "assets/shared-components/contact-modal.html"); 
 });
+
+// Contact form titles
+function setModalHeader(title, secTitle){
+    document.getElementById("modalTitle").innerHTML = title;
+
+    if(secTitle === undefined){} else{
+        document.getElementById("secondModalTitle").innerHTML = secTitle;
+    }
+}
+
+// Tawk API
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ce553f82846b90c57afddba/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
