a5e24e27ba9a2d8f61bc65bce8847d56755a5ba1
jackperrotta <tug24657@temple.edu>
2019-07-23 19:26:03 -0400

    add new navbar and new blog post

--- assets/scripts/shared-components.js
+++ assets/scripts/shared-components.js
@@ -17,12 +17,12 @@ function setModalHeader(title, secTitle = null){
 }
 
 // Tawk API
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5ce553f82846b90c57afddba/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/5ce553f82846b90c57afddba/default';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
